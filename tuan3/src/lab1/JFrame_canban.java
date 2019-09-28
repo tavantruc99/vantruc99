cc5c918a39376765c176e5a4c36444f6ac2d13fa
tavantruc99 <55102092+tavantruc99@users.noreply.github.com>
2019-09-28 14:03:25 +0700

    tuan3

---
+++ tuan3/src/lab1/JFrame_canban.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author E5-575G
+ */
+public class JFrame_canban extends JFrame{
+    
+    public JFrame_canban(){
+        setVisible(true);
+    }
+    
+    public static void main(String[] args) {
+        new JFrame_canban();
+    }
+}
